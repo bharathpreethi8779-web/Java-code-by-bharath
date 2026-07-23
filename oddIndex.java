cf881a1964877f635f2e2e096a0fbf0f7250659f
bharathpreethi8779-web <bharathpreethi8779@gmail.com>
2026-07-23 16:58:07 +0530

    Create oddIndex.java

---
+++ oddIndex.java
@@ -0,0 +1,22 @@
+public class oddIndex{
+    public static void main(String[] args) {
+        int[]arr = {10,20,30,40,50};
+        System.out.println("oddIndex");
+        for(int i=0;i<arr.length;i++)
+        {
+            if(i%2==1)
+            System.out.print(arr[i]+" ");
+            System.out.println("oddIndex");
+            for(int i=0;i<arr.length;i++)
+        }
+            System.out.println();
+            System.out.print("Even Index:");
+            for(int i=0;i<arr.length;i++)
+                if(i%2==0)
+                {
+                    System.out.println();
+                    
+                }
+            }
+        }
+    
