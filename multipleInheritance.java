c736006f7dd79d2583f0563a2f7a59c6087b4706
bharathpreethi8779-web <bharathpreethi8779@gmail.com>
2026-07-27 16:24:06 +0530

    Create multipleInheritance.java

---
+++ multipleInheritance.java
@@ -0,0 +1,20 @@
+interface camera{
+    void photo();
+
+}
+interface music{
+    void playMusic();
+}
+class mobile implements camera, music{
+    public void photo(){
+        System.out.println("photo taken");
+    }
+    public void playMusic(){
+         System.out.println("music playing");
+    }
+}
+public class Main{
+      public static void main (String[] args) {
+    mobile m = new mobile();
+}
+}
